d00823069fe3bc154709c63077feaaf7720f8691
Akin94 <akin.tutkan@gmail.com>
2023-06-08 01:06:13 +0300

    data fetching next

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Layout from '../components/layout';
 
-export default function Home() {
+export default function Home({movies}) {
+  console.log(movies)
   return (
     <>
    
@@ -14,21 +15,32 @@ export default function Home() {
       </Head>
 
       <main>
-        {/* <Link href="/about">
-          Hakkında
-        </Link>
-        <Link href="/blog">
-          Blog
-        </Link>
-        <Link href="/blog/post">
-          Post
-        </Link>
-
-        <Image src="/image/ak.jpeg" width="300" height="300"/> */}
-        <h3>Anasayfa</h3>
+      
+        <h3>Popüler Filmler</h3>
+        {movies.results.map(movie =>(
+          <div>
+            <h3>{movie.title}</h3>
+          </div>
+        ))}
       </main>
 
 
     </>
   )
+}
+export async function getStaticProps(){
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer 440811ce73c5e267143f1803ee10a9fa'
+  //   }
+  // };
+  const request = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=440811ce73c5e267143f1803ee10a9fa&language=en-US&page=1')
+  const movies = await request.json();
+  return{
+    props:{
+      movies
+    }
+  }
 }
